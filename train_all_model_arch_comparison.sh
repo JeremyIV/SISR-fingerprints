ca0beb1c4658f9acfe0bf455c88a8cb0bdd33a3d
guppy <guppy@pdx.edu>
2022-10-03 11:53:50 -0700

    cleaning up extraneous files

--- train_all_model_arch_comparison.sh
+++
@@ -1,4 +0,0 @@
-python classification/train_classifier.py -opt classification/options/ConvNext_CNN_SISR_all_models.yml --mode train
-python classification/train_classifier.py -opt classification/options/EfficientNet_CNN_SISR_all_models.yml --mode train
-python classification/train_classifier.py -opt classification/options/ResNet50_CNN_SISR_all_models.yml --mode train
-python classification/train_classifier.py -opt classification/options/Xception_CNN_SISR_all_models.yml --mode train
--- train_prnu_parsers_withholding_nothing.sh
+++
@@ -1,3 +0,0 @@
-python classification/train_classifier.py -opt classification/options/parsers/PRNU_SISR_architecture_parser.yaml --mode train
-python classification/train_classifier.py -opt classification/options/parsers/PRNU_SISR_loss_parser.yaml --mode train
-python classification/train_classifier.py -opt classification/options/parsers/PRNU_SISR_scale_parser.yaml --mode train
